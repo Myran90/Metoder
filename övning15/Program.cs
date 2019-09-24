4ae05c5a3c3310c97231e83229d1eb62f11eec17
Myran <myrankalander@gmail.com>
2019-09-24 10:58:33 +0200

    Övning 15 klar

---
+++ övning15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace övning15
+{
+    class Program
+    {
+        static string HurPåDig(string hej, string mår)
+        {
+            string hurMårDu = hej+mår;
+            return hurMårDu;
+        }
+        static void Main(string[] args)
+        {
+           string meddelande = HurPåDig("Hej på dig!", " Hur mår du?");
+           Console.WriteLine(meddelande);
+        }
+    }
+}
