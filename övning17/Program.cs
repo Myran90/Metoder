7af760d6a84fb72f5d3848133fe506f6590cd9d7
Myran <myrankalander@gmail.com>
2019-09-24 11:18:05 +0200

    Övning 17 klar

---
+++ övning17/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace övning17
+{
+    class Program
+    {
+        static double Moms(double moms)
+        {
+            double addera = moms*1.25;
+            return addera;
+        }
+        static void Main(string[] args)
+        {
+            double summa = Moms(100);
+            Console.WriteLine(summa);
+        }
+    }
+}
