bfb78e6bb150b152d730a0e6c0379fedd67cf2c7
Myran <myrankalander@gmail.com>
2019-09-24 11:07:16 +0200

    Övning 16 klar

---
+++ övning16/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace övning16
+{
+    class Program
+    {
+        static int Addera(int tal1, int tal2)
+        {
+            int addera = tal1+tal2;
+            return addera;
+        }
+        static void Main(string[] args)
+        {
+            int summa = Addera(5, 1);
+            Console.WriteLine(summa);
+        }
+    }
+}
