2c0944453d3b86efb87bcd5785cca05ab337b5c3
Myran <myrankalander@gmail.com>
2019-09-24 10:43:06 +0200

    Övning 14 klar

---
+++ övning14/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace övning14
+{
+    class Program
+    {
+        static void Hello(string hej)
+        {
+            Console.WriteLine(hej);
+        }
+        static void Main(string[] args)
+        {
+            Hello("Hej på dig!");
+        }
+    }
+}
